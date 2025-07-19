b10af55e8c3227bddf63874c5b6291b5204f4f19
RafaelLimaJ <rafaellimanf05@gmail.com>
2025-07-18 21:24:03 -0300

    l,

--- PYTHON_CASA/Exercicios23.py
+++ PYTHON_CASA/Exercicios23.py
@@ -1,7 +1,17 @@
 while True:
     try:
-        idade = int(input("Digite sua idade"))
-        print (f"Você tem {idade} anos")
+        num1 = int(input("Digite o primeiro numero"))
+        num2 = int(input("Digite o segundo numero"))
+
+        conta1 = num1 + num2 
+        conta2 = num1 - num2
+        conta3 = num1 * num2
+        conta4 = num1 / num2
+
+        print(f"{num1} + {num2} = {conta1}\n{num1} - {num2} = {conta2}\n{num1} * {num2} = {conta3}\n {num1} / {num2} = {conta4}")
         break
-    except:
+    except ValueError:
         print("Digite apenas numeros")
+    except ZeroDivisionError:
+        print(f"{num1} + {num2} = {conta1}\n{num1} - {num2} = {conta2}\n{num1} * {num2} = {conta3}")
+        print(f"Nao é possivel dividir {num1} por 0")
